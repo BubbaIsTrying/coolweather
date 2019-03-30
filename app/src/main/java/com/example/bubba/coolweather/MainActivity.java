51fcdc5d679ffd3899eaebf0349ab6d67beba65e
Bubba <13413887133w@sina.cn>
2019-03-30 21:50:08 +0800

    创建数据库和数据表的配置

---
+++ app/src/main/java/com/example/bubba/coolweather/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.bubba.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bubba on 2019/3/30.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    
+}
